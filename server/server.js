3464a19e101f6126880793e671d4a5b5d90eb646
Dinesh Mittal <Dinesh.Mittal@infostretch.com>
2019-06-19 10:11:51 +0530

    ADD post todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,100 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+
+var {mongoose}=require('./db/mongoose');
+var {Todo}=require('./models/Todo');
+var {Users}=require('./models/Users');
+
+var app=express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+
+var todo=new Todo({
+  text:req.body.text
+});
+
+todo.save().then((doc)=>{res.send(doc);},(e)=>{res.status(400).send(e);})
+
+})
+
+
+app.post('/user',(req,res)=>{
+
+  var todo=new Todo({
+    text:req.body.text
+  })
+
+  todo.save().then((doc)=>{res.send(doc);},(e)=>{res.send(e);})
+})
+
+app.listen(3000,()=>{
+  console.log('Ready to listen on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //
+// //
+// //
+// //
+// // // var newTodo=new Todo({text:'cook dinner'});
+// //
+// // // var newTodo=new Todo({text:'Done Homework',completed:true,completedAt:12.8})
+// //
+// // var newTodo=new Todo({text:'Edit the vedio'})
+// //
+// // newTodo.save().then((doc)=>{console.log('Saved todo',doc);},(e)=>{'unable to save todo'});
+//
+//
+//
+//
+//
+// var newUser=new User({email:"  dinesh.mittal@gmail.com  "});
+//
+//
+// newUser.save().then((docs)=>{console.log('Saved user data',docs);},(err)=>{console.log('cannot save user data');});
