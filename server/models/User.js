ec4978f230dd3314d0b71e23181ab6753b8dd84c
Dinesh Mittal <Dinesh.Mittal@infostretch.com>
2019-06-21 14:54:38 +0530

    Add delete /user/me/token route

--- server/models/User.js
+++ server/models/User.js
@@ -51,7 +51,10 @@ UserSchema.methods.generateAuthToken=function(){
     return  user.save().then(()=>{return token;});
 };
 
-
+UserSchema.methods.removeToken=function(token){
+  var user=this;
+  return user.update({$pull:{token:token}});
+};
 
 
 UserSchema.statics.findByToken=function(token){
