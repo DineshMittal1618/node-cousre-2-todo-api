ba09399a35acf4323b0f84840d8f264a9fc27e1b
Dinesh Mittal <Dinesh.Mittal@infostretch.com>
2019-06-21 13:23:54 +0530

    ADD post /users/login route

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,23 +1,20 @@
  const {SHA256}=require('crypto-js');
  const jwt=require('jsonwebtoken');
+ const bcrypt=require('bcryptjs');
+
+ var password='123abc!';
+
+ bcrypt.genSalt(10,(err,salt)=>{
+   bcrypt.hash(password,salt,(err,hash)=>{
+     console.log(hash);
+   });
+ });
 
 
- var data={id:10};
-
- var token=jwt.sign(data,'123abc');
- console.log(token);
- var decode=jwt.verify(token,'123abc');
-
-console.log(decode);
-
-
-
-
-
-
-
-
-
+ bcrypt.compare('123!',hashedPassword,(err,res)=>{
+var hashedPassword='$2a$10$x5UrjDwIKIy8X9DuHzud8.PNaB4kiQDSiGPSpYzboV2Nb0y7oJfSq';
+  console.log(res);
+});
 
 
 
@@ -31,6 +28,38 @@ console.log(decode);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  var data={id:10};
+//
+//  var token=jwt.sign(data,'123abc');
+//  console.log(token);
+//  var decode=jwt.verify(token,'123abc');
+//
+// console.log(decode);
+//
 //
 // var message='i am user number 3';
 //
--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 var mongoose=require('mongoose');
 const validator=require('validator');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
 const _=require('lodash');
 var UserSchema=new mongoose.Schema({
@@ -50,6 +51,9 @@ UserSchema.methods.generateAuthToken=function(){
     return  user.save().then(()=>{return token;});
 };
 
+
+
+
 UserSchema.statics.findByToken=function(token){
   var user =this;
   var decoded;
@@ -69,6 +73,46 @@ UserSchema.statics.findByToken=function(token){
     'tokens.access':'auth'
   });
 };
+
+UserSchema.statics.findByCredentials=function(email,password){
+  var user=this;
+
+  return user.findOne({email}).then((user)=>{
+    if(!user)
+    {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve,reject)=>{
+      bcrypt.compare(password,user.password,(err,res)=>{
+        if(res){
+          resolve(user);
+        }
+        else{
+          reject();
+        }
+      });
+    });
+
+  });
+};
+
+UserSchema.pre('save',function(next){
+  var user=this;
+
+  if(user.isModified('password')){
+
+    bcrypt.genSalt(10,(err,salt)=>{
+      bcrypt.hash(user.password,salt,(err,hash)=>{
+        user.password=hash;
+        next();
+      });
+    });
+  }
+  else{
+    next();
+  }
+});
 
 var users=mongoose.model('users',UserSchema);
 
